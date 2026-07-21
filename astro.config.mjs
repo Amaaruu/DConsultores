d6ceef11493d911c66ffd34e23df68dde6364420
Isaac González <isaac.gonza1210@gmail.com>
2026-07-21 17:36:03 -0400

    se adapta a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL ?? 'https://www.dconsultores.cl',
-  adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
